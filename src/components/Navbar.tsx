c2645664652e41dd51bc390a8d70d09c79df2caf
Sumiara5365 <rafiq15-5365@diu.edu.bd>
2026-09-13 20:28:42 +0600

    respo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,82 @@
+import { useState } from "react";
 import Logo from "../assets/logo-text.png";
 import "./Navbar.css";
 
-
-
 function Navbar() {
-    return (
-        <nav className="navbar">
-          
-       <img src={Logo} alt="Logo" />
-        
-        <div className="nav-links">
-         
-        <a href="#">Home</a>
-        <a href="#technologies">Technologies</a>
-        <a href="#projects">Projects</a>
-        <a href="#about">About</a>
-        <a href="#contact">Contact</a>
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  return (
+    <nav className="navbar">
+
+      
+      <button
+        className="hamburger"
+        onClick={() => setMenuOpen(!menuOpen)}
+        aria-label="Toggle menu"
+        aria-expanded={menuOpen}
+      >
+        {menuOpen ? "✕" : "☰"}
+      </button>
+
+    
+      <div className="logo">
+        <img src={Logo} alt="Dev Stack Logo" />
       </div>
 
+    
+      <div className="nav-links">
+        <a href="#" onClick={closeMenu}>Home</a>
+        <a href="#technologies" onClick={closeMenu}>
+          Technologies
+        </a>
+        <a href="#projects" onClick={closeMenu}>
+          Projects
+        </a>
+        <a href="#about" onClick={closeMenu}>
+          About
+        </a>
+        <a href="#contact" onClick={closeMenu}>
+          Contact
+        </a>
+      </div>
+
+      
+      {menuOpen && (
+        <div className="mobile-menu">
+          <a href="#" onClick={closeMenu}>
+            Home
+          </a>
+
+          <a href="#technologies" onClick={closeMenu}>
+            Technologies
+          </a>
+
+          <a href="#projects" onClick={closeMenu}>
+            Projects
+          </a>
+
+          <a href="#about" onClick={closeMenu}>
+            About
+          </a>
+
+          <a href="#contact" onClick={closeMenu}>
+            Contact
+          </a>
+        </div>
+      )}
+
+      
       <div className="nav-buttons">
         <button className="sign-in">Sign In</button>
         <button className="sign-up">Sign Up</button>
       </div>
+
     </nav>
-    );
-};
+  );
+}
 
 export default Navbar;
